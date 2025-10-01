add4b590f0885448bdb493fe2d0c55f7e3af0f7d
adityapaul26 <adityapaul2603@gmail.com>
2025-10-01 19:42:09 +0530

    Navbar-1-add

---
+++ src/components/Navbar/Navbar1.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Navbar1=()=>{
+    return(
+        <>
+            <nav className='bg-gray-900 text-white p-4'>
+                <ul className='flex flex-wrap items-center justify-center gap-8'>
+                    <li className='inline-flex items-center text-white rounded-2xl border border-gray-400 cursor-pointer px-7 py-2 bg-[#432323] transition-colors duration-300 ease-out hover:text-gray-900 hover:bg-[#C2A68C]'>Start</li>
+                    <li className='inline-flex items-center rounded-2xl border border-gray-400 px-7 py-2 cursor-pointer transition-colors duration-300 ease-out hover:bg-gray-800'>About me</li>
+                    <li className='inline-flex items-center rounded-2xl border border-gray-400 px-7 py-2 cursor-pointer transition-colors duration-300 ease-out hover:bg-gray-800'>Skills</li>
+                    <li className='inline-flex items-center rounded-2xl border border-gray-200 px-7 py-2 cursor-pointer transition-colors duration-300 ease-out hover:bg-gray-800'>Projects</li>
+                    <li className='inline-flex items-center rounded-2xl border border-gray-400 px-7 py-2 cursor-pointer transition-colors duration-300 ease-out hover:bg-gray-800'>Contact</li>
+                </ul>
+            </nav>
+        </>
+    )
+}
